f1f8bea2542e39f2ecc6ce6ff310a4d3d4e1aa9d
naomehmi <125885797+naomehmi@users.noreply.github.com>
2023-09-27 16:52:01 +0700

    Update main.py

--- files/main.py
+++ files/main.py
@@ -1,4 +1,4 @@
-from minimarketClasses import product, consumable, nonConsumable, customer, employee, minimarket, stock
+from minimarketClasses import *
 from time import sleep
 from random import randrange
 import datetime
@@ -26,7 +26,7 @@ def tutorialLevel():
         if interact != "1":
             print("=> Press '1'")
     print()
-    stock.showStock(unlocked,player.employeeName,True)
+    stock.showStock(unlocked,True)
 
 def mainMenu():
     print("="*75)
@@ -35,6 +35,8 @@ def mainMenu():
     print("|{:^73}|".format("MINIMARKET SIMULATOR"))
     sleep(0.03)
     print("|{:^73}|".format("*"*20))
+    sleep(0.03)
+    print("|{:^73}|".format("created by cool"))
     sleep(0.03)
     print("|{:^73}|".format(" "))
     sleep(0.03)
@@ -82,7 +84,7 @@ def mainMenu():
             print("=> Press 'p' or 'q'")
     if interact == 'p':
         print()
-        print("{:^74}".format("Loading"))
+        print("{:^74}".format("Loading..."))
         print("{:^22}".format(" "),end="")
         for i in range(30):
             print("∎",end="")
@@ -94,12 +96,16 @@ menu = mainMenu()
 miniMarket = minimarket(1000, 3, 1, 1)
 stock = stock([[],[],[],[],[],[],[],[]],10)
 unlocked = 2
+cond = ["GOOD", "BAD", "GOOD","GOOD","GOOD", "GOOD", "GOOD", "GOOD"]
 for i in range(7):
-    stock.listofProducts[0].append(consumable("RA-759","APPLE",3.00,"PCS","GOOD",datetime.datetime(2023,12,1)))
+    stock.listofProducts[0].append(consumable("AP-"+str(randrange(1000,9999)),"APPLE",3.00,"PCS","GOOD","1 week"))
+stock.listofProducts[0].append(consumable("AP-"+str(randrange(1000,9999)),"APPLE",3.00,"PCS","BAD","1 week"))
 for i in range(10):
-    stock.listofProducts[1].append(nonConsumable("MK-011","MILK",3.00,"PCS","GOOD"))
+    stock.listofProducts[1].append(nonConsumable("MK-"+str(randrange(1000,9999)),"MILK",3.00,"PCS",cond[randrange(1,1000)%8]))
+print("=" * 75)
+print()
 if menu == 'p':
-    print("=> Welcome to Minimarket Simulator. In this game, you will be playing as an minimarket employee.")
+    print("=> Welcome to Minimarket Simulator, newbie. In this game, you will be playing as an minimarket employee.")
     print("=> Your tasks include: processing customer payments, restocking products, and managing the minimarket's finances.")
     print("=> The game will be over if you get fired or quit your job. Are you ready?")
     print()
@@ -108,7 +114,7 @@ if menu == 'p':
     player = employee("EMPLOYEE"+str(randrange(1000,9999)),input("=> ").title())
     print()
     interact = None
-    print("=> Would you like to play our interactive tutorial? (Y/N)")
+    print(f"=> Would you like to play our interactive tutorial, {player.employeeName}? (Y/N)")
     while interact != "y" and interact != "n":
         interact = input("=> ").lower()
         print()
