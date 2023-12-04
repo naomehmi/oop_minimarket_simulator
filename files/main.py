bff5ed979997fa50c118cc22695c0eefd96fa4a3
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-12-04 17:00:09 +0700

    Create main.py

---
+++ files/main.py
@@ -0,0 +1,14 @@
+from classes.people import *
+from classes.productsAndStock import *
+from classes.theGame import *
+
+"""
+fitur yang belum selesai:
+1. exp date
+2. main level (shift) => in progress
+3. stats review (stlh quit/resign)
+4. formatting tabel (blm rapi)
+"""
+
+game = MINIMARKET()
+game.gameplay()
