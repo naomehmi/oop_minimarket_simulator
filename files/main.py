4235b8c20380b91b4baf34f975af3950e28ac6ed
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-12-05 23:32:33 +0700

    simplified it

--- files/main.py
+++ files/main.py
@@ -1,13 +1,5 @@
 from classes.theGame import *
 
-"""
-fitur yang belum selesai:
-1. exp date (maybe with observer pattern?) [x] => method expire mirip yg notify di modul
-2. main level (shift) => in progress [x]
-3. stats review (stlh quit/resign) []
-4. make interface more comfortable and easy to use [idk si]
-5. money [x]
-"""
-
+# ONLY RUN THIS FILE
 game = MINIMARKET()
 game.gameplay()
