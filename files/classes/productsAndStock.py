9cc396ab188fef3743717163025effc994d46072
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-12-05 13:26:18 +0700

    added expire() function so products can now expire

--- files/classes/productsAndStock.py
+++ files/classes/productsAndStock.py
@@ -2,16 +2,7 @@ from time import sleep
 from random import randrange
 import abc
 
-
-# FITUR YANG BELUM
-# ================
-# sistem manajemen keuangan (game ini bisa beli2 tp gk ada uanganya AWKWKWK)
-# Design pattern gabung employee dan customer => employee.shiftGameplay(variabelCustomer) misal
-# RAPIKAN FUNCTION GENERATEPRODUCTS() DI CLASS STOCK, BISA PAKAI DESIGN PATTERN UNTUK MERAPIKANNYA, jadi lgsg kek mis generateProducts(consumable(....))
-# validasi agar customer tidak mengambil product yang bad/expired
-# fak help me hiks
-
-#class abstract product
+# class abstract product
 class Product(metaclass=abc.ABCMeta):
 	@abc.abstractproperty
 	def code(self):
@@ -63,24 +54,29 @@ class Stock:
 	maxCapacity = 10
 	unlocked = 2
 	_allProducts = [
-		{ "con" : 1 ,"code" : "AP", "name" : "APPLE", "price" : 3.00, "uom" : "PCS", "expDate" : "7 days" }, 
-		{ "con" : 1 ,"code" : "MK", "name" : "MILK", "price" : 3.45, "uom" : "PCS", "expDate" : "7 days" }, 
-		{ "con" : 1 ,"code" : "EG", "name" : "EGGS", "price" : 7.50, "uom" : "CARTONS", "expDate" : "10 days" }, 
-		{ "con" : 0 ,"code" : "TS", "name" : "TISSUE", "price" : 5.00, "uom" : "PCS" }, 
-		{ "con" : 1 ,"code" : "OV", "name" : "OLIVE OIL", "price" : 9.95, "uom" : "BOTTLE" , "expDate" : "15 days"}, 
-		{ "con" : 0 ,"code" : "CH", "name" : "WOODEN CHAIR", "price" : 25.56, "uom" : "PCS" }, 
-		{ "con" : 0 ,"code" : "TB", "name" : "WOODEN TABLE", "price" : 57.49, "uom" : "PCS" }, 
-		{ "con" : 1 ,"code" : "RC", "name" : "RICE", "price" : 15.46, "uom" : "BAGS", "expDate" : "20 days" }
+		{ "con" : 1 ,"code" : "AP", "name" : "APPLE", "price" : 3.00, "uom" : "PCS", "expDate" : "7 days", "cost" : 2.00 }, 
+		{ "con" : 1 ,"code" : "MK", "name" : "MILK", "price" : 3.45, "uom" : "PCS", "expDate" : "7 days", "cost" : 3.23 }, 
+		{ "con" : 1 ,"code" : "EG", "name" : "EGGS", "price" : 7.50, "uom" : "CARTONS", "expDate" : "10 days", "cost" : 6.68 }, 
+		{ "con" : 0 ,"code" : "TS", "name" : "TISSUE", "price" : 5.00, "uom" : "PCS", "cost" : 3.40 }, 
+		{ "con" : 1 ,"code" : "OV", "name" : "OLIVE OIL", "price" : 9.95, "uom" : "BOTTLE" , "expDate" : "15 days", "cost" : 7.77 }, 
+		{ "con" : 0 ,"code" : "CH", "name" : "WOODEN CHAIR", "price" : 25.56, "uom" : "PCS", "cost" : 20.55 }, 
+		{ "con" : 0 ,"code" : "TB", "name" : "WOODEN TABLE", "price" : 57.49, "uom" : "PCS", "cost" : 55.55 }, 
+		{ "con" : 1 ,"code" : "RC", "name" : "RICE", "price" : 15.46, "uom" : "BAGS", "expDate" : "20 days", "cost" : 12.23 }
 	]
 
 	@property
 	def shelf(self):
 		return self._listOfProducts
+	
+	@property
+	def products(self):
+		return self._allProducts
 
 	def generateProducts(self, idx, qty):
 		tmp = self._allProducts[idx].copy()
 		conOrNon = tmp["con"]
 		tmp.pop("con")
+		tmp.pop("cost")
 		if conOrNon:
 			for i in range(qty): self._listOfProducts[idx].append(Consumable(**tmp))
 		else:
@@ -89,8 +85,18 @@ class Stock:
 	def removeProducts(self, idx, row):
 		self._listOfProducts[idx].pop(row)
 
-	def displayStock(self):
+	def expire(self):
+		for i in self._listOfProducts:
+			if not i: break
+			if i[0].__class__.__name__ == "Consumable":
+				for j in i:
+					if j.expDate != "EXPIRED" :
+						day = int(j.expDate[:2].rstrip()) - 1
+						j.expDate = "EXPIRED" if day <= 0 else str(day) + " days"
+
+	def displayStock(self, money):
 		while True:
+			print("\nMONEY : $" + "%.2f" % money)
 			print("-"*60)
 			print("|{:^3}|{:^15}|{:^12}|{:^25}|".format("No.","Product Name", "Total Stock","Price per Unit (USD)"))
 			print("|{:^3}|{:^15}|{:^12}|{:^25}|".format("-"*3,"-"*15,"-"*12,"-"*25))
@@ -106,18 +112,20 @@ class Stock:
 			try:
 				interact = int(input("=> "))
 				if not 0 <= interact <= self.unlocked: raise ValueError
-				if interact == 0: break
-				self.restockItems(interact-1)
+				if interact == 0: return money
+				money = self.restockItems(interact-1, money)
+				if money < 0: return money
 			except ValueError:
 				print(f"Press a number between 1-{self.unlocked}\n")
 
-	def restockItems(self, idx):
+	def restockItems(self, idx, money):
 		while True:
 			prod = self._allProducts[idx]
 			className = "Consumable" if prod["con"] else "nonConsumable"
-			try: 
-				print()
-				print("Product :",prod["name"])
+			try:
+				print("\nMONEY : $" + "%.2f" % money) 
+				print("\nProduct :",prod["name"])
+				print(f"PRICE : ${'%.2f' % self._allProducts[idx]['cost']}/{self._allProducts[idx]['uom']}")
 				print("-"*34,end="")
 				if className == "Consumable": print("-"*16,end="")
 				print()
@@ -160,26 +168,28 @@ class Stock:
 							if qty > self.maxCapacity - amt:
 								err = f"You can only buy up to {self.maxCapacity - amt} for now"
 								raise ValueError
-							# if uang nya gak cukup raise value error sini juga ya
+							money -= self._allProducts[idx]["cost"] * qty
 							self.generateProducts(idx, qty) if className == "Consumable" else self.generateProducts(idx, qty)
-							print(f"\n=> {qty} product(s) have been added.")
+							print(f"\n=> {qty} product(s) have been added."), sleep(0.8)
 							break
 						except ValueError:
 							print(err + "\n")
 				elif interact == 2:
 					if amt == 0:
-						print("There are no products to discard. I think you should buy more instead")
+						print("There are no products to discard. I think you should buy more instead"), sleep(0.6)
 						continue
 					while True:
 						try:
-							print(f"Which row do you want to discard? (1-{amt})")
+							print(f"Which row do you want to discard (1-{amt})? Pick '0' to go back to the previous menu")
 							row = int(input("=> "))
-							if not 1 <= row <= amt: raise ValueError
-							print(f"Product {prod[row-1].code} has been discarded.")
+							if not 0 <= row <= amt: raise ValueError
+							if row == 0: break
+							print(f"Product {prod[row-1].code} has been discarded."), sleep(0.8)
 							self.removeProducts(idx, row-1)
 							break
 						except ValueError:
-							print(f"Input a number between 1-{amt}")
+							print(f"Input a number between 1-{amt}\n")
 				elif interact == 3: break
 			except ValueError:
 				print("Press '1', '2', or '3'.")
+		return money
