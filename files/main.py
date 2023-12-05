095e505b2557210f1321706f1d3a4a5ff079cdbf
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-12-05 13:23:23 +0700

    erased import, feature checklist

--- files/main.py
+++ files/main.py
@@ -1,13 +1,12 @@
-from classes.people import *
-from classes.productsAndStock import *
 from classes.theGame import *
 
 """
 fitur yang belum selesai:
-1. exp date
-2. main level (shift) => in progress
-3. stats review (stlh quit/resign)
-4. formatting tabel (blm rapi)
+1. exp date (maybe with observer pattern?) [x] => method expire mirip yg notify di modul
+2. main level (shift) => in progress [x]
+3. stats review (stlh quit/resign) []
+4. make interface more comfortable and easy to use [idk si]
+5. money [x]
 """
 
 game = MINIMARKET()
