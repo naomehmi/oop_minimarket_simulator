77927e95b147016965ebdba06d89e2e792677896
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-01 19:35:11 +0700

    added testing

--- files/main.py
+++ files/main.py
@@ -1,5 +1,10 @@
-from classes.theGame import *
+from theGame import *
+from tester import *
 
 # ONLY RUN THIS FILE
-game = MINIMARKET()
-game.gameplay()
+
+if __name__ == "__main__":
+  # unittest.main(verbosity=2)
+
+  game = MINIMARKET()
+  game.gameplay()
