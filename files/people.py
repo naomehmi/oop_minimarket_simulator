11888e30890087c36427f34a58cfd41baddb1bb3
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-07 17:40:47 +0700

    made employeeNameCheck() into a separate class for SRP

--- files/people.py
+++ files/people.py
@@ -3,11 +3,17 @@ from datetime import datetime
 from time import sleep
 from random import randrange
 from abc import ABC, abstractmethod
+from printFormat import PRINT73
+from os import system
 
 # customer class (SOLID - Single Responsibility)
 class Customer:
 	def __init__(self):
-		self.cart = []
+		self._cart = []
+
+	@property
+	def getCustItems(self):
+		return self._cart
 	
 	# fill in customer's cart with random products
 	def fillCart(self, stock):
@@ -31,16 +37,15 @@ class Customer:
 				if i >= len(tmp): # if all items of that product are bad/expired, remove that product from available
 					available.pop(x)
 					continue
-				self.cart.append(tmp[i]) # if a good and unexpired item is available 
+				self._cart.append(tmp[i]) # if a good and unexpired item is available 
 				stock.removeProducts(available[x] - 1, i) # remove that product from stock because it's taken
 				taken += 1
-		self.cart = sorted(self.cart, key = lambda x : x.name) # sort cart alphabetically
+		self._cart = sorted(self._cart, key = lambda x : x.name) # sort cart alphabetically
 		return True
 	
 	def printCart(self): # string representation of customer
-		print("Customer's items:")
-		sleep(0.3)
-		for i in self.cart:
+		print("Customer's items:"), sleep(0.3)
+		for i in self._cart:
 			print("{:<10} {:<10} {:<10}".format(i.code, i.name, i.price)), sleep(0.3)
 		print()
 
@@ -61,8 +66,8 @@ class InputItems(Task):
 		print(f"\nMISTAKES : {self.player.mistake} / 3\n") # current mistakes
 		self.customer.printCart(), sleep(0.6) # print customer cart
 
-		items = {i.name : 0 for i in self.customer.cart} # this dictionary is to get the quantity of the items in the customer's cart
-		for i in self.customer.cart: items[i.name] += 1
+		items = {i.name : 0 for i in self.customer.getCustItems} # this dictionary is to get the quantity of the items in the customer's cart
+		for i in self.customer.getCustItems: items[i.name] += 1
 
 		allItems = [self.stock.products[i]["name"] for i in range(self.stock.unlocked)] # every product name that the player has unlocked
 		allPrices = [self.stock.products[i]["price"] for i in range(self.stock.unlocked)] # every product price that the player has unlocked
@@ -84,7 +89,7 @@ class InputItems(Task):
 					"The customer does not have that product in their cart",
 					"That is the wrong number of quantity of this item",
 					"You have already input that product before."
-				] # possible error messages
+				] # possible error messages when valueError raised
 				idx = 0
 				if self.player.mistake >= 3: return # game over if player makes 3 mistakes
 				it = int(input(f"PRODUCT (1 - {self.stock.unlocked}) : ")) # player input which product is in the customer's cart
@@ -118,47 +123,31 @@ class PrintReceipt(Task):
 	def __init__(self, receipts, player):
 		self.receipts = receipts
 		self.player = player
+		self.header = [
+			"COOL MINIMARKET",
+			datetime.now().strftime("%c"),
+			f"Cashier : {self.player.code} - {self.player.name}"
+		]
 
 	def execute(self):
 		total = 0 # total cost
-		# PRINT RECEIPT
 		print("="*75), sleep(0.03)
-		print("|{:^73}|".format(" ")), sleep(0.03)
-		print("|{:^73}|".format("COOL MINIMARKET")), sleep(0.03)
-		print("|{:^73}|".format(" ")), sleep(0.03)
-		print("|{:^73}|".format(datetime.now().strftime("%c"))), sleep(0.03)
-		print("|{:^73}|".format(" ")), sleep(0.03)
-		print("|{:^73}|".format(f"Cashier : {self.player.code} - {self.player.name}")), sleep(0.03)
-		print("|{:^73}|".format(" ")), sleep(0.03)
+		for i in self.header: PRINT73().value(" "), PRINT73().value(i)
+		PRINT73().value(" ")
 		for i in self.receipts:
 			for x, y in i.items():
 				total += y["qt"] * y["pr"]
 				print("|{:<10}{:<25}{:17} x ${:<5}{:>12}|".format(" ", x, y["qt"], "%.2f" % y["pr"] ," ")), sleep(0.03)
-		print("|{:^73}|".format(" ")), sleep(0.03)
+		PRINT73().value(" ")
 		print("|{:<10}{:<52}+{:>10}|".format(" ", "="*50, " ")), sleep(0.03)
-		print("|{:^73}|".format(" ")), sleep(0.03)
+		PRINT73().value(" ")
 		print("|{:<10}{:<7} : {:>41}{:>12}|".format(" ", "TOTAL", "$" + str("%.2f" % total), " ")), sleep(0.03)
-		print("|{:^73}|".format(" ")), sleep(0.03)
+		PRINT73().value(" ")
 		print("="*75)
 		return total
 
 class CashExchange(Task):
-	def __init__(self, total, player):
-		self.total = total
-		self.player = player
-
-	def execute(self):
-		lowestMultipleOf5 = math.ceil(self.total / 5) * 5 # round up the total cost to the nearest multiple of 5
-		customerPaid = randrange(lowestMultipleOf5, 10 * ((lowestMultipleOf5 // 10) + 1) + 1, 5) # the customer pays in a randomized amount of dollars between the nearest multiple of 5 up to the nearest multiple of 10
-		print(f"\nCUSTOMER'S CASH : ${customerPaid}\n")
-		if "%.2f" % customerPaid == "%.2f" % self.total: # if customer pays the exact amount of money
-			print("Oh, the customer has paid the exact amount, so no change needed.")
-		else:
-			print("Give the customer the correct amount of change to finish the payment.")
-			changeNeeded = (customerPaid - self.total) # change
-			taken = 0.00 # change taken from the cash register
-			cashInHand = []
-			money = [ # all the money in the cash register
+	MONEY = [ # all the money in the cash register
 				{"name" : "1 cent", "value" : 0.01},
 				{"name" : "5 cent", "value" : 0.05},
 				{"name" : "10 cent", "value" : 0.10},
@@ -169,12 +158,28 @@ class CashExchange(Task):
 				{"name" : "10 dollars", "value" : 10.00},
 				{"name" : "50 dollars", "value" : 50.00},
 				{"name" : "100 dollars", "value" : 100.00},
-			]
+	]
+
+	def __init__(self, total, player):
+		self.total = total
+		self.player = player
+		self.cashInHand = []
+		self.taken = 0.00 # change taken from the cash register
+		lowestMultipleOf5 = math.ceil(self.total / 5) * 5 # round up the total cost to the nearest multiple of 5
+		self.customerPaid = randrange(lowestMultipleOf5, 10 * ((lowestMultipleOf5 // 10) + 1) + 1, 5) # the customer pays in a randomized amount of dollars between the nearest multiple of 5 up to the nearest multiple of 10
+		self.changeNeeded = (self.customerPaid - self.total) # change
+
+	def execute(self):
+		print(f"\nCUSTOMER'S CASH : ${self.customerPaid}\n")
+		if "%.2f" % self.customerPaid == "%.2f" % self.total: # if customer pays the exact amount of money
+			print("Oh, the customer has paid the exact amount, so no change needed.")
+		else:
+			print("Give the customer the correct amount of change to finish the payment.")
 			while True:
 				try:
-					print(f"\nCash taken : {cashInHand}, total = {'%.2f' % taken}")
-					print(f"\nChange needed for customer : {'%.2f' % changeNeeded}")
-					if taken: # if some money has already been taken out, give player the option to put back money or hand customer money
+					print(f"\nCash taken : {self.cashInHand}, total = {'%.2f' % self.taken}")
+					print(f"\nChange needed for customer : {'%.2f' % self.changeNeeded}")
+					if self.taken: # if some money has already been taken out, give player the option to put back money or hand customer money
 						print("\nPick an action to continue (1-3)")
 						print("1. Take more money from cash register")
 						print("2. Put back money into cash register")
@@ -187,9 +192,9 @@ class CashExchange(Task):
 						while True:
 							try: # player picks which type of money to give to customer
 								print("\nPick 1 - 10 to select which bill/coin to take from the cash register", end="")
-								print(", or pick '0' to go back to the previous menu") if cashInHand else print()
-								for i in range(1,6): print("{:<3} {:<7} | {:<2} {:<12}".format(str(i) + ".", money[i-1]["name"], str(i + 5) + ".", money[i + 5 - 1]["name"]))
-								interact = int(input("=> "))
+								print(", or pick '0' to go back to the previous menu") if self.cashInHand else print()
+								for i in range(1,6): print("{:<3} {:<7} | {:<2} {:<12}".format(str(i) + ".", self.MONEY[i-1]["name"], str(i + 5) + ".", self.MONEY[i + 5 - 1]["name"])) # print all the dollar cents and bill available
+								interact = int(input("=> ")) 
 								if not 0 <= interact <= 10: raise ValueError("Pick a number between 1-10")
 								if interact == 0 : break
 								unit = "bills" if interact > 5 else "coins"
@@ -197,16 +202,16 @@ class CashExchange(Task):
 									try:
 										# player picks quantity of the chosen money value to take
 										e = "pick a number above 0" # error message
-										print(f"\nHow many {money[interact - 1]['name']} {unit} do you want to take from the cash register? (Pick '0' to cancel)")
+										print(f"\nHow many {self.MONEY[interact - 1]['name']} {unit} do you want to take from the cash register? (Pick '0' to cancel)")
 										qty = int(input("=> "))
 										if not 0 <= qty : raise ValueError(e)
 										if qty == 0: break # return to previous menu if input 0
-										if money[interact - 1]["value"] * qty > changeNeeded + 10 or qty > 50: # if player takes out too much money at a time, raise valuerror
+										if self.MONEY[interact - 1]["value"] * qty > self.changeNeeded + 10 or qty > 50: # if player takes out too much money at a time, raise valuerror
 											e = "I think that's too much money, pick a smaller amount"
 											raise ValueError("I think that's too much money, pick a smaller amount")
 										for i in range(qty):
-											cashInHand.append(money[interact - 1]["name"]) # append the money taken from cash register
-										taken += money[interact - 1]["value"] * qty
+											self.cashInHand.append(self.MONEY[interact - 1]["name"]) # append the money taken from cash register
+										self.taken += self.MONEY[interact - 1]["value"] * qty
 										break
 									except ValueError:
 										print(str(e) + "\n")
@@ -216,53 +221,52 @@ class CashExchange(Task):
 					# put back money into cash register
 					elif action == 2:
 						tmp = {}
-						for i in range(len(cashInHand)) : tmp[i+1] = cashInHand[i]
+						for i in range(len(self.cashInHand)) : tmp[i+1] = self.cashInHand[i]
 						print()
 						for x, y in tmp.items(): print(f"{x}. {y}") # print all the money the player has taken out from the cash register
-						print(f"\nWhich dollar bill/coin do you want to put back into the cash register (1-{len(cashInHand)})? or pick '0' to go back to the previous menu.")
+						print(f"\nWhich dollar bill/coin do you want to put back into the cash register (1-{len(self.cashInHand)})? or pick '0' to go back to the previous menu.")
 						while True:
 							try:
 								it = int(input("=> "))
-								if not 0 <= it <= len(cashInHand): raise ValueError
+								if not 0 <= it <= len(self.cashInHand): raise ValueError
 								if it == 0: break
-								for i in money: # deduct amount of money taken by the value of the money being put back inside 
-									if i["name"] == cashInHand[it - 1]: taken -= i["value"]
-								cashInHand.pop(it-1) # remove that money from list
+								for i in self.MONEY: # deduct amount of money taken by the value of the money being put back inside 
+									if i["name"] == self.cashInHand[it - 1]: self.taken -= i["value"]
+								self.cashInHand.pop(it-1) # remove that money from list
 								break
-							except ValueError:
-								print(f"pick a number between 1-{len(cashInHand)}")
+							except ValueError: print(f"pick a number between 1-{len(self.cashInHand)}")
 					# hand customer the change
 					elif action == 3:
-						if '%.2f' % taken == '%.2f' % changeNeeded: # if the returned change is correct, customer interaction is completed
-							print("\nThe amount of change you have returned is correct.\nThe customer has left the minimarket.")
+						if '%.2f' % self.taken == '%.2f' % self.changeNeeded: # if the returned change is correct, customer interaction is completed
+							print("\nThe amount of change you have returned is correct.\nThe customer has left the minimarket."), sleep(1)
 							break
 						else: # if wrong, mistake + 1
 							print("\nThe amount of change you have given is incorrect, try again.")
 							self.player.mistake += 1
 							print(f"=> MISTAKES : {self.player.mistake} / 3")
 							if self.player.mistake == 3: return 
-				except ValueError:
-					print("Input a number between 1-3")
-		return
+					system('cls')
+				except ValueError: print("Input a number between 1-3")
 
 # InputItems + PrintReceipt + CashExchange => Each implements Single Responsibility Principle because they have their own task
+
+class EmployeeNameValidation:
+	def check(self):
+		print("=> Before we start, what's your name? (Numbers, spaces, and symbols are not allowed)")
+		try:
+			name = input("=> ")
+			if not name.isalpha(): raise ValueError("=> Numbers, spaces, and symbols are not allowed. Please try again :)\n")
+		except ValueError as e:
+			print(str(e))
+			return self.check()
+		return name.title()
 	
 # player
 class Employee:
 	def __init__(self):
 		self.code = "EMPLOYEE"+str(randrange(1000,10000)) # randomized employee code
 		self.mistake = 0
-
-	def EmployeeNameCheck(self): # recursive function to validate employee name
-			print("\n\n=> Before we start, what's your name? (Numbers, spaces, and symbols are not allowed)")
-			try:
-				name = input("=> ")
-				if not name.isalpha():
-						raise ValueError("=> Numbers, spaces, and symbols are not allowed. Please try again :)\n")
-			except ValueError as e:
-				print(str(e))
-				return self.EmployeeNameCheck()
-			self.name = name.title()
+		self.name = None
 	
 	# inputting items, printing receipts, and cash exchange
 	def ProcessPayment(self, customer, stock):
@@ -270,5 +274,6 @@ class Employee:
 		if self.mistake >= 3: return
 		else:
 			receipts = temp
+			sleep(0.55), system('cls')
 			total = PrintReceipt(receipts,self).execute() # print receipt
 			CashExchange(total,self).execute() # cash exchange
