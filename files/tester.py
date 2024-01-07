9fe9a5d5aab07951238f062426c456cf6b1b518c
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-07 14:55:41 +0700

    added testQuantity

--- files/tester.py
+++ files/tester.py
@@ -2,14 +2,23 @@ import unittest
 from people import Customer
 from productsAndStock import Stock
 
-class checkProductCondition(unittest.TestCase):
-  def testing(self):
-    """checking whether every product in customer's cart is in GOOD condition"""
-    c = Customer()
+class Testing(unittest.TestCase):
+  def sampleStock(self):
     s = Stock()
     s.generateProducts(0, 8)
     s.generateProducts(1, 10)
-    c.fillCart(s)
+    s.generateProducts(2,7)
+    return s
+  
+  def testQuantity(self):
+    """checking whether the quantity of the generated product is correct"""
+    self.sampleStock().generateProducts(5, 6)
+    self.assertEqual(len(self.s.shelf[5]),6)
+
+  def testCondition(self):
+    """checking whether every product in customer's cart is in 'GOOD' condition"""
+    c = Customer()
+    c.fillCart(self.sampleStock())
     for i in c.cart:
       with self.subTest("Condition",i=i):
         self.assertEqual(i.condition,"GOOD")
