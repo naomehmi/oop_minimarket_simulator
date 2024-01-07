316b985c2da656b9ad2e397895b33ab07c680ae5
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-07 17:37:30 +0700

    added a comment

--- files/main.py
+++ files/main.py
@@ -2,9 +2,10 @@ from theGame import *
 from tester import *
 
 # ONLY RUN THIS FILE
+# reccomended to run in an IDE that supports os.system('cls'), such as VS Code
 
 if __name__ == "__main__":
   # unittest.main(verbosity=2)
 
   game = MINIMARKET()
-  game.gameplay()
+  game.mainMenu()
