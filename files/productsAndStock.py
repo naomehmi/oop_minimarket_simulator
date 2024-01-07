430c1d3981d0477576b29d7d71d906f884489234
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2024-01-07 14:55:05 +0700

    Stock is now properly and observer class, and apply SOLID principles into code

--- files/productsAndStock.py
+++ files/productsAndStock.py
@@ -2,66 +2,57 @@ from time import sleep
 from random import randrange
 from abc import ABC, abstractmethod
 
-# abstract class as template for consumable and non consumable (template pattern ?? go ask chatgpt)
+# abstract class as template for consumable and non consumable (template pattern) + acts as Observer
 class Product(ABC):
+	def __init__(self, code, name, price, uom):
+		self.code = code + "-" + str(randrange(1000, 10000))
+		self.name = name
+		self.price = price
+		self.uom = uom
+		self.condition = "BAD" if randrange(1, 10) == 1 else "GOOD"
+
 	@property
 	@abstractmethod
-	def code(self):
+	def expDate(self):
 		pass
-	@property
+	
+	# observer
 	@abstractmethod
-	def name(self):
-		pass
-	@property
-	@abstractmethod
-	def price(self):
-		pass
-	@property
-	@abstractmethod
-	def uom(self):
-		pass
-	@property
-	@abstractmethod
-	def condition(self):
+	def update(self):
 		pass
 
 # products with expiry dates
 class Consumable(Product):
-	code = ""
-	name= ""
-	price = 0
-	uom = ""
-	condition = ""
+	expDate = ""
 	def __init__(self, code, name, price, uom, expDate):
-		self.code = code + "-" + str(randrange(1000,10000))
-		self.name = name
-		self.price = price
-		self.uom = uom
-		self.condition = "BAD" if randrange(0,9) == 1 else "GOOD" # there is a 1 in 9 chance the produuct is in bad condition
+		super().__init__(code, name, price, uom)
 		self.expDate = expDate
 
+	def update(self):
+		if self.expDate == "EXPIRED" : self.condition = "BAD" # condition becomes bad if item is expired
+
 # products without expiry dates
-class nonConsumable(Product):
-	code = ""
-	name= ""
-	price = 0
-	uom = ""
-	condition = ""
+class NonConsumable(Product):
 	def __init__(self, code, name, price, uom):
-		self.code = code + "-" + str(randrange(1000,10000))
-		self.name = name
-		self.price = price
-		self.uom = uom
-		self.condition = "BAD" if randrange(0,9) == 1 else "GOOD"
+		super().__init__(code, name, price, uom)
+
+	@property
+	def expDate(self):
+		return None
+	
+	def update(self):
+		pass
+		
+# Product + Consumable + NonConsumable => Liskov Substitution Principle
 		
 # class to control every product in the minimarket, has an observer design pattern
-# observer : stock, observed : consumable/non consumable
+# observable : stock, observer : consumable/non consumable
 class Stock:
-	_listOfProducts = [[] for _ in range(8)] # the minimarket sells 8 diff products
+	_listOfProducts = [[] for _ in range(8)] # the minimarket sells 8 diff products, this is to store the observers
 	maxCapacity = 10 # maximum amount of items per product, will increase as player levels up
 	unlocked = 2 # the types of products that the minimarket sells for now, will increase as player levels up
 	_allProducts = [
-		{ "con" : 1 ,"code" : "AP", "name" : "APPLE", "price" : 3.00, "uom" : "PCS", "expDate" : "5 days", "cost" : 2.30 }, 
+		{ "con" : 1 ,"code" : "AP", "name" : "APPLE", "price" : 30.00, "uom" : "PCS", "expDate" : "5 days", "cost" : 2.30 }, 
 		{ "con" : 1 ,"code" : "MK", "name" : "MILK", "price" : 3.45, "uom" : "PCS", "expDate" : "6 days", "cost" : 2.95 }, 
 		{ "con" : 1 ,"code" : "EG", "name" : "EGGS", "price" : 7.50, "uom" : "CARTONS", "expDate" : "10 days", "cost" : 6.68 }, 
 		{ "con" : 0 ,"code" : "TS", "name" : "TISSUE", "price" : 5.00, "uom" : "PCS", "cost" : 4.40 }, 
@@ -80,30 +71,36 @@ class Stock:
 	def products(self):
 		return self._allProducts
 	
-	# to add items into the stock
+	# to add items into the stock (add observer)
 	def generateProducts(self, idx, qty):
 		tmp = self._allProducts[idx].copy() # copy the dictionary of the product we want to generate from _listOfProducts
 		conOrNon = tmp["con"] # store the value of con (either 0 or 1)
-		# remove con and cost key from the dictionary so making a new consumable/nonconsumable instance won't cause an error
+		# remove con and cost key from the dictionary so making a new consumable/Nonconsumable instance won't cause an error
 		tmp.pop("con")
 		tmp.pop("cost")
 		if conOrNon: # conOrNon = 1 generate consumable
-			for i in range(qty): self._listOfProducts[idx].append(Consumable(**tmp))
+			for _ in range(qty): self._listOfProducts[idx].append(Consumable(**tmp))
 		else: # conOrNon = 0 generate non consumable
-			for i in range(qty) : self._listOfProducts[idx].append(nonConsumable(**tmp))
+			for _ in range(qty) : self._listOfProducts[idx].append(NonConsumable(**tmp))
 
-	# to discard items from the stock
+	# to discard items from the stock (remove observer)
 	def removeProducts(self, idx, row):
 		self._listOfProducts[idx].pop(row)
 
-	# every consumable product's expiry date minus 1 day
+	# notify
+	def ChangeCondition(self):
+		for i in self._listOfProducts:
+			for j in i:
+				j.update()
+
+	# every consumable product's expiry date minus 1 day (update products)
 	def expire(self):
 		iterator = iter(self._listOfProducts)
 		while True:
 			try:
 				i = next(iterator)
 				if not i: break
-				if i[0].__class__.__name__ == "Consumable":
+				if isinstance(i[0], Consumable):
 					item = iter(i)
 					while True:
 						try:
@@ -111,13 +108,17 @@ class Stock:
 							if j.expDate != "EXPIRED" :
 								day = int(j.expDate[:2].rstrip()) - 1 # take the number of days left from .expDate and convert it to int
 								j.expDate = "EXPIRED" if day <= 0 else str(day) + " days" # product becomes expired after reaching 0 days
-								if j.expDate == "EXPIRED" : j.condition = "BAD" # condition becomes bad if item is expired
+							self.ChangeCondition()	
 						except StopIteration:
 							break
 			except StopIteration:
 				break
 
-	# stock control
+# SOLID - Open/Closed Principle + Liskov Substitution Principle + Dependency Inversion (karena bergantung sm class product(abstract))
+class StockControl:
+	def __init__(self, stock):
+		self.stock = stock
+
 	def displayStock(self, money):
 		while True:
 			print("\nMONEY : $" + "%.2f" % money)
@@ -126,27 +127,26 @@ class Stock:
 			print("|{:^3}|{:^15}|{:^12}|{:^25}|".format("No.","Product Name", "Total Stock","Price per Unit (USD)"))
 			print("|{:^3}|{:^15}|{:^12}|{:^25}|".format("-"*3,"-"*15,"-"*12,"-"*25))
 			idx = 1
-			for i in range(self.unlocked):
-				prod = self._listOfProducts[i]
-				print("|{:^3}|{:^15}|{:^12}|{:^25}|".format(idx,self._allProducts[i]["name"], str(len(prod)) + " " + self._allProducts[i]["uom"], "%.2f" % self._allProducts[i]["price"]))
+			for i in range(self.stock.unlocked):
+				prod = self.stock.shelf[i]
+				print("|{:^3}|{:^15}|{:^12}|{:^25}|".format(idx,self.stock.products[i]["name"], str(len(prod)) + " " + self.stock.products[i]["uom"], "%.2f" % self.stock.products[i]["price"]))
 				idx += 1
 			print("-"*60)
-			print(f"\nPress '0' to go back to the main menu, or press a number between 1-{self.unlocked} to check each item of the product.")
+			print(f"\nPress '0' to go back to the main menu, or press a number between 1-{self.stock.unlocked} to check each item of the product.")
 			# if player presses 0, goes back to main game in theGame.py. if the player selects the row of a product, goes to restockItems()
 			try:
 				interact = int(input("=> "))
-				if not 0 <= interact <= self.unlocked: raise ValueError
+				if not 0 <= interact <= self.stock.unlocked: raise ValueError
 				if interact == 0: return money
 				money = self.restockItems(interact-1, money)
 				if money < 0: return money
-			except ValueError:
-				print(f"Press a number between 1-{self.unlocked}\n")
+			except ValueError: print(f"Press a number between 1-{self.stock.unlocked}\n")
 
-	# this is where player can buy or discard items of a product
+	# this is where the player can buy or discard items of a product
 	def restockItems(self, idx, money):
 		while True:
-			prod = self._allProducts[idx] # the dictionary of product we want to add/discard
-			className = "Consumable" if prod["con"] else "nonConsumable" 
+			prod = self.stock.products[idx] # the dictionary of product we want to add/discard
+			className = "Consumable" if prod["con"] else "NonConsumable" 
 			try: # format table
 				print("\nMONEY : $" + "%.2f" % money) 
 				print("\nProduct :",prod["name"])
@@ -159,7 +159,7 @@ class Stock:
 				if className == "Consumable": print("{:^15}|".format("-"*15),end="")
 				print("{:^12}|".format("-"*12))
 				j = 1
-				prod = self._listOfProducts[idx]
+				prod = self.stock.shelf[idx]
 				amt = len(prod)
 				iterator = iter(prod)
 				while True: # print items
@@ -183,22 +183,22 @@ class Stock:
 				if not 1 <= interact <= 3: raise ValueError
 				print()
 				if interact == 1: # buy product
-					if amt == self.maxCapacity: # cannot buy if the quantity of items has already reached the max capacity
-						print(f"There is already {self.maxCapacity} items of this product. You cannot buy more than {self.maxCapacity} items for now.")
+					if amt == self.stock.maxCapacity: # cannot buy if the quantity of items has already reached the max capacity
+						print(f"There is already {self.stock.maxCapacity} items of this product. You cannot buy more than {self.stock.maxCapacity} items for now.")
 						continue
 					while True:
 						err = "Please input a number more than 0." # default error message
 						try:
-							print(f"Quantity : {amt}\nCurrent max capacity : {self.maxCapacity}") # quantity of items to buy
-							print(f"Cost to Buy : ${'%.2f' % self._allProducts[idx]['cost']}/{self._allProducts[idx]['uom']}")
+							print(f"Quantity : {amt}\nCurrent max capacity : {self.stock.maxCapacity}") # quantity of items to buy
+							print(f"Cost to Buy : ${'%.2f' % self.stock.products[idx]['cost']}/{self.stock.products[idx]['uom']}")
 							print("\nHow many products do you want to add?")
 							qty = int(input("=> "))
-							if qty > self.maxCapacity - amt: # cannot buy more than max capacity
-								err = f"You can only buy up to {self.maxCapacity - amt} for now" # change error message
+							if qty > self.stock.maxCapacity - amt: # cannot buy more than max capacity
+								err = f"You can only buy up to {self.stock.maxCapacity - amt} for now" # change error message
 								raise ValueError
-							cost = self._allProducts[idx]["cost"] * qty
+							cost = self.stock.products[idx]["cost"] * qty
 							money -= cost # money is subtracted by cost of items
-							self.generateProducts(idx, qty) # add the items into the stock
+							self.stock.generateProducts(idx, qty) # add the items into the stock
 							print(f"(-${'%.2f' % cost})\n\n=> {qty} product(s) have been added."), sleep(0.8)
 							break
 						except ValueError:
@@ -214,7 +214,7 @@ class Stock:
 							if not 0 <= row <= amt: raise ValueError
 							if row == 0: break
 							print(f"Product {prod[row-1].code} has been discarded."), sleep(0.8)
-							self.removeProducts(idx, row-1)
+							self.stock.removeProducts(idx, row-1)
 							break
 						except ValueError:
 							print(f"Input a number between 1-{amt}\n")
